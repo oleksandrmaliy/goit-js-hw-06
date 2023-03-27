6c667ae02ee48122a0e3f0bd3833ee0fec3dc1d9
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-03-27 22:31:36 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email === '' || password === '') {
+    const alertMsg = 'All fields must be filled in!';
+    alert(alertMsg);
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+
+    console.log(formData);
+
+    formEl.reset();
+  }
+}
