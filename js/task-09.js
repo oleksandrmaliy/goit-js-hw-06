e9829042c42799e99f78e84facf3b25f6f7be459
oleksandrmaliy <115729512+oleksandrmaliy@users.noreply.github.com>
2023-03-28 00:15:29 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanColorNumber = document.querySelector('.color');
+
+buttonEl.addEventListener('click', changeBodyColor);
+
+function changeBodyColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanColorNumber.textContent = getRandomHexColor();
+}
